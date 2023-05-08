62ab7e41aaea688997014cac62ef7cabea912b85
fishst1k <ben@fishmill.io>
2023-05-08 08:07:02 -0600

    minor rename

--- fishbits.dev/terraform/modules/route53/main.tf
+++ fishbits.dev/terraform/modules/route53/main.tf
@@ -1,8 +1,8 @@
-resource "aws_route53_zone" "route53_zone" {
+resource "aws_route53_zone" "route53" {
   name = var.zone_name
 }
 
-resource "aws_route53_record" "route53_zone" {
+resource "aws_route53_record" "route53" {
   for_each = {for each in var.records: each.name => each}
 
   zone_id = aws_route53_zone.route53_zone.zone_id
