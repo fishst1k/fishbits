0f0f77db304c7f36c9824380739fd6059bb9feaf
fishst1k <ben@fishmill.io>
2023-05-08 09:42:49 -0600

    fix naming

--- fishbits.dev/terraform/modules/route53/main.tf
+++ fishbits.dev/terraform/modules/route53/main.tf
@@ -5,7 +5,7 @@ resource "aws_route53_zone" "route53" {
 resource "aws_route53_record" "route53" {
   for_each = {for each in var.records: each.name => each}
 
-  zone_id = aws_route53_zone.route53_zone.zone_id
+  zone_id = aws_route53_zone.route53.zone_id
   name = each.value.name
   type = each.value.type
   ttl = each.value.ttl
